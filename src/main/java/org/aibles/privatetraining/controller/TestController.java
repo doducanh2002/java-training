f7864a363a1ffa0e90f92285c052f9b2426388d7
doducanh2002 <anh15042002hla@gmail.com>
2024-04-18 11:57:09 +0700

    fix: fix error 401

--- src/main/java/org/aibles/privatetraining/controller/TestController.java
+++ src/main/java/org/aibles/privatetraining/controller/TestController.java
@@ -2,12 +2,6 @@ package org.aibles.privatetraining.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aibles.privatetraining.dto.request.UserRequest;
-import org.aibles.privatetraining.dto.response.AuthenticationResponse;
-import org.aibles.privatetraining.service.UserProfileService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,32 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TestController {
 
-    @Autowired
-    private UserProfileService userService;
-
-    @GetMapping("")
-    public String test() {
+    @GetMapping("/test/1")
+    public String testHello1() {
         return "hello";
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticate(@RequestBody UserRequest userRequest) {
-        try {
-            AuthenticationResponse authenticationResponse = userService.authenticate(userRequest);
-            return ResponseEntity.ok(authenticationResponse);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect username or password");
-        }
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody UserRequest userRequest) {
-        log.info("(register)userRequest: {}", userRequest);
-        try {
-            userService.register(userRequest);
-            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: " + e.getMessage());
-        }
+    @GetMapping("/test/2")
+    public String testHello2() {
+        return "hello";
     }
 }
